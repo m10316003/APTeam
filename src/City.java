3ad0b094c9b33f37286a0ec6f357aa8b580534bf
m10316003 <m10316003@go.utaipei.edu.tw>
2015-04-27 18:35:13 +0800

    1040427 update TSP1

---
+++ src/City.java
@@ -0,0 +1,21 @@
+public class City {
+private double x,y;
+
+public double getX() {
+	return x;
+}
+
+public void setX(double x) {
+	this.x = x;
+}
+
+public double getY() {
+	return y;
+}
+
+public void setY(double y) {
+	this.y = y;
+}
+
+}
+
